3331122386c87e94ad3cc8ae63a62996fb438add
George Mihov <gmihov001@gmail.com>
2020-12-07 18:36:29 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,7 @@
 
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
-
-// window.onload = function() {
-//   console.log("Hello Rigo from the console!");
-// };
 
 const validate = e => {
   e.preventDefault();
